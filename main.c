110ed3955c16c53f6ec5fbf3226035427014ae37
nahvun <nahyun@sookmyung.ac.kr>
2022-11-15 16:55:20 +0900

    11_05

--- main.c
+++ main.c
@@ -5,21 +5,21 @@
 
 
 int main(void){
-	int i;
-	int grade[5];
-	int accum = 0;
 	
-	for(i=0;i<5;i++){
-		printf("grade[%i] = ", i);
-		scanf("%d", &grade[i]);
-	}
+	char *pc;
+	int *pi;
+	double *pd;
 	
-	for(i=0;i<5;i++){
-		printf("%i : %i\n", i, *(grade+i));
-		accum += *(grade+i);
-			
-	}
+	pc = (char *)10000;
+	pi = (int *)10000;
+	pd = (double *)10000;
 	
-	printf("average : %d\n", accum/5);
+	printf("증가 전 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
+	
+	pc++;
+	pi++;
+	pd++;
+	printf("증가 후 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
+	
+	return 0;
 }
-
